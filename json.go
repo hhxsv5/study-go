93fdae4d619fbdde3c107beaddf2d14e0d96a1bb
XieBiao <xiebiao@medlinker.com>
2017-09-07 19:01:40 +0800

    json encode/decode

---
+++ json.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Dog struct {
+	Id    uint32 //大写字母开头的为public，外部可见
+	Name  string
+	Age   uint8
+	price float32 //小写字母开头的为protected，整个package可见
+}
+
+func NewDog(id uint32, name string, age uint8, price float32) *Dog {
+	return &Dog{id, name, age, price}
+}
+
+func main() {
+
+	//Json Encode
+	dog1 := NewDog(1, "Dave", 10, 1000.00)
+	json1, _ := json.Marshal(dog1)
+	jsonstr1 := string(json1)
+	fmt.Println(jsonstr1) //price不可见
+
+	//Json Decode
+	var dog2 Dog
+	err := json.Unmarshal([]byte(jsonstr1), &dog2)
+	fmt.Println(err, dog2)
+}
